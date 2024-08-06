864ca867df1694197bca6dc3281d91e36ede35b1
nakshatra raghav <combatrickshaw@gmail.com>
2024-08-06 08:43:44 +0530

    (backend): request validation middleware sends exact fields missing or invalid

--- backend/internal/middlewares/validate.go
+++ backend/internal/middlewares/validate.go
@@ -2,8 +2,10 @@ package middlewares
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/nakshatraraghav/transcodex/backend/lib"
 	"github.com/nakshatraraghav/transcodex/backend/types"
 	"github.com/nakshatraraghav/transcodex/backend/util"
@@ -22,7 +24,22 @@ func ValidateRequestBody[schema any](next http.Handler) http.Handler {
 		vd := lib.GetValidator()
 
 		if err := vd.Struct(body); err != nil {
-			util.WriteError(w, http.StatusBadRequest, err.Error())
+			verr, ok := err.(validator.ValidationErrors)
+			if !ok {
+				util.WriteError(w, http.StatusBadRequest, "request validation errors")
+				return
+			}
+
+			fields := make(map[string]string)
+
+			for _, e := range verr {
+				fields[e.Field()] = getErrorMessage(e)
+			}
+
+			util.WriteError(w, http.StatusBadRequest, map[string]any{
+				"fields": fields,
+			})
+
 			return
 		}
 
@@ -32,4 +49,19 @@ func ValidateRequestBody[schema any](next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
+
+func getErrorMessage(e validator.FieldError) string {
+	switch e.Tag() {
+	case "required":
+		return "this field is required"
+	case "email":
+		return "invalid email format, please fix it"
+	case "min":
+		return fmt.Sprintf("should be at least %s characters long", e.Param())
+	case "max":
+		return fmt.Sprintf("should be at most %s characters long", e.Param())
+	default:
+		return "invalid value"
+	}
 }
