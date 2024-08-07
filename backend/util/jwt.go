35098f8fd29421b1ce3d25f051cd4d2d5c347bc2
nakshatra raghav <combatrickshaw@gmail.com>
2024-08-07 13:47:24 +0530

    (backend): add jwt fields to the env file validation

---
+++ backend/util/jwt.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/nakshatraraghav/transcodex/backend/internal/config"
+)
+
+type Tokens struct {
+	AccessToken  string
+	RefreshToken string
+}
+
+func CreateTokens(payload jwt.MapClaims) (*Tokens, error) {
+	var tokens Tokens
+
+	env := config.GetEnv()
+
+	attl, _ := time.ParseDuration(env.ACCESS_TOKEN_TTL)
+	rttl, _ := time.ParseDuration(env.REFRESH_TOKEN_TTL)
+
+	tok, err := create(payload, attl, env.JWT_PRIVATE_KEY)
+	if err != nil {
+		return nil, err
+	}
+
+	tokens.AccessToken = tok
+
+	tok, err = create(payload, rttl, env.JWT_PRIVATE_KEY)
+	if err != nil {
+		return nil, err
+	}
+
+	tokens.RefreshToken = tok
+
+	return &tokens, nil
+}
+
+func create(payload jwt.MapClaims, ttl time.Duration, key string) (string, error) {
+
+	payload["exp"] = time.Now().Add(ttl).Unix()
+
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+
+	return t.SignedString(key)
+}
