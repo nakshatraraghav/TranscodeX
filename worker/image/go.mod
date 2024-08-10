602dfd164039f55da75e20ab5181c8884e0a042e
nakshatra raghav <combatrickshaw@gmail.com>
2024-08-10 17:57:02 +0530

    (worker): refactoring in progress

--- worker/image/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/nakshatraraghav/transcodex/worker/image
-
-go 1.22.5
--- worker/image/internal/app/app.go
+++
@@ -1,7 +0,0 @@
-package app
-
-type Application struct{}
-
-func NewImageWorker() *Application {
-	return &Application{}
-}
