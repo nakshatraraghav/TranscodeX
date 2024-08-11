7ab8258e453427deb5d04a44dee64a91c5f4d419
nakshatra raghav <combatrickshaw@gmail.com>
2024-08-11 16:29:58 +0530

    (worker): refactor app package

--- worker/cmd/main.go
+++ worker/cmd/main.go
@@ -1,29 +1,21 @@
 package main
 
 import (
-	"context"
 	"log/slog"
 	"os"
 
-	"github.com/nakshatraraghav/transcodex/worker/config"
-	"github.com/nakshatraraghav/transcodex/worker/internal/s3"
+	"github.com/nakshatraraghav/transcodex/worker/internal/application"
 )
 
 func main() {
-	if err := config.LoadEnv(); err != nil {
-		slog.Error(err.Error())
-		os.Exit(1)
-	}
-
-	service, err := s3.NewS3Service()
+	app, err := application.NewApp()
 	if err != nil {
 		slog.Error(err.Error())
 		os.Exit(1)
 	}
 
-	err = service.Upload(context.Background())
+	err = app.Run()
 	if err != nil {
 		slog.Error(err.Error())
 	}
-
 }
---
+++ worker/internal/application/application.go
@@ -0,0 +1,73 @@
+package application
+
+import (
+	"context"
+
+	"github.com/nakshatraraghav/transcodex/worker/config"
+	"github.com/nakshatraraghav/transcodex/worker/internal/processors/audio"
+	"github.com/nakshatraraghav/transcodex/worker/internal/processors/image"
+	"github.com/nakshatraraghav/transcodex/worker/internal/processors/video"
+	"github.com/nakshatraraghav/transcodex/worker/internal/s3"
+)
+
+type MediaProcessor interface {
+	LoadData() error
+	ApplyTransformations(map[string]string) ([]byte, error)
+}
+
+type Application struct {
+	processor *MediaProcessor
+	service   *s3.S3Service
+}
+
+func NewApp() (*Application, error) {
+
+	err := config.LoadEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	env := config.GetEnv()
+
+	s, err := s3.NewS3Service()
+	if err != nil {
+		return nil, err
+	}
+
+	var p MediaProcessor
+
+	switch env.MEDIA_TYPE {
+	case "IMAGE":
+		p = image.NewImageProcessor()
+	case "VIDEO":
+		p = video.NewVideoProcessor()
+	case "AUDIO":
+		p = audio.NewAudioProcessor()
+	default:
+		p = nil
+	}
+
+	return &Application{
+		service:   s,
+		processor: &p,
+	}, nil
+}
+
+func (a *Application) Run() error {
+
+	// Step 1: Download the media file from S3
+	err := a.service.Download(context.Background())
+	if err != nil {
+		return err
+	}
+
+	// Step 2: run transformations on it
+
+	// Step 3: upload the s3
+	err = a.service.Upload(context.Background())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
