30f48607566b8644b38f0d1ed68b7ebd3c287f34
nakshatra raghav <combatrickshaw@gmail.com>
2024-08-13 22:04:46 +0530

    (worker): database conn

---
+++ worker/db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log/slog"
+
+	_ "github.com/lib/pq"
+	"github.com/nakshatraraghav/transcodex/worker/config"
+)
+
+func NewPostgresConnection() (*sql.DB, error) {
+	env := config.GetEnv()
+
+	db, err := sql.Open("postgres", env.CONNECTION_STRING)
+	if err != nil {
+		slog.Info("failed to connect to the pg database")
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		slog.Info("failed to ping database")
+		return nil, err
+	}
+
+	slog.Info("connected to the database successfully")
+	return db, nil
+}
